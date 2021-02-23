8a25ccad395b9177d38a443cfaee0aff49121054
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2021-02-24 05:14:23 +0530

    quotly.py
    This was feature request which i also felt imp link to feature request - https://t.me/UltroidRequests/67     all credits goes to telebot developers and I have just edited !! no significant changes have been made !! hope so added in add ons

---
+++ quotly.py
@@ -0,0 +1,51 @@
+# credits @telebot and @maxprogrammer007 for editing
+# ported from <https://github.com/xditya/TeleBot/blob/master/telebot/plugins/quotly.py>
+# Ported for Ultroid < https://github.com/TeamUltroid/Ultroid >   
+"""QuotLy: Avaible commands: .qbot
+"""
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from uniborg.util import admin_cmd
+
+from ultroid import CMD_HELP
+
+
+@ultroid.on(admin_cmd(pattern="qbot ?(.*)"))
+@ultroid.on(sudo_cmd(pattern="qbot ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await eor(event, "```Reply to any user message.```")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.text:
+        await eor(event, "```Reply to text message```")
+        return
+    chat = "@QuotLyBot"
+    reply_message.sender
+    if reply_message.sender.bot:
+        await eor(event, "```Reply to actual users message.```")
+        return
+    await eor(event, "```Making a Quote```")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1031952739)
+            )
+            await event.client.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
+            return
+        if response.text.startswith("Hi!"):
+            await eor(
+                event,
+                "```Can you kindly disable your forward privacy settings for good?```",
+            )
+        else:
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
+
+
+CMD_HELP.update({"quotly": ".qbot <reply to message>\nUse - To make a quote."})
