7fad2818737c5e0a37373f5b39b09a15fdf99fc2
maxxy231 <79555582+maxxy231@users.noreply.github.com>
2021-02-24 08:23:27 +0530

    Much better. Merge it....

--- iloveyou.py
+++ iloveyou.py
@@ -1,16 +1,20 @@
-""plugin for ur gf
-
-\nCode by @maxprogrammer007 (orginally by @H1M4N5HU0P )for @theultroid
+# Plugin for ur gf
+# Code by @maxprogrammer007 (orginally by @H1M4N5HU0P )for @theultroid
 # Ported for Ultroid < https://github.com/TeamUltroid/Ultroid >   
+# Type .iloveyou to see the love.
 
-type .iloveyou to see the love.
 """
+✘ Commands Available -
+• `{i}iloveyou`
+    Show your love.
+"""
+
 import random, re
 from uniborg.util import admin_cmd
 import asyncio
 from telethon import events
 
-@borg.on(admin_cmd(pattern="iloveyou ?(.*)"))
+@borg.on(admin_cmd(pattern="iloveyou$"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         await event.edit("""😘😘😘😘😘😘😘😘
@@ -105,3 +109,6 @@ async def _(event):
   😘😘                  😘😘
       😘😘😘😘😘😘
             😘😘😘😘""")
+
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
