5551cc8c7b3f3ce233cb5db5b238f89ea09a7916
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-02-24 17:49:22 +0530

    help remain

--- quotly.py
+++ quotly.py
@@ -5,47 +5,37 @@
 """
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from uniborg.util import admin_cmd
 
-from ultroid import CMD_HELP
+from . import *
 
 
-@ultroid.on(admin_cmd(pattern="qbot ?(.*)"))
-@ultroid.on(sudo_cmd(pattern="qbot ?(.*)", allow_sudo=True))
+@ultroid_cmd(pattern="quotly")
 async def _(event):
-    if event.fwd_from:
-        return
     if not event.reply_to_msg_id:
-        await eor(event, "```Reply to any user message.```")
-        return
+        return await eor(event, "```Reply to any user message.```")
     reply_message = await event.get_reply_message()
     if not reply_message.text:
-        await eor(event, "```Reply to text message```")
-        return
+        return await eor(event, "```Reply to text message```")
     chat = "@QuotLyBot"
     reply_message.sender
-    if reply_message.sender.bot:
-        await eor(event, "```Reply to actual users message.```")
-        return
-    await eor(event, "```Making a Quote```")
-    async with event.client.conversation(chat) as conv:
+    ac = await eor(event, "```Making a Quote```")
+    async with ultroid_bot.conversation(chat) as conv:
         try:
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1031952739)
             )
-            await event.client.forward_messages(chat, reply_message)
+            await ultroid_bot.forward_messages(chat, reply_message)
             response = await response
         except YouBlockedUserError:
-            await event.reply("```Please unblock me (@QuotLyBot) u Nigga```")
-            return
+            return await event.reply("```Please unblock @QuotLyBot and try again```")
         if response.text.startswith("Hi!"):
             await eor(
                 event,
                 "```Can you kindly disable your forward privacy settings for good?```",
             )
         else:
-            await event.delete()
-            await event.client.send_message(event.chat_id, response.message)
+            await ac.delete()
+            await ultroid_bot.send_message(event.chat_id, response.message)
 
 
-CMD_HELP.update({"quotly": ".qbot <reply to message>\nUse - To make a quote."})
+HELP.update({"quotly": ".qbot <reply to message>\nUse - To make a quote."})
