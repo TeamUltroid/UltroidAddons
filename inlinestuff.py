dee8e5d6d9e6a10b30a2fda91f3fd7a55d3e6c62
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-02-25 08:53:22 +0530

    tweet

---
+++ inlinestuff.py
@@ -0,0 +1,39 @@
+#
+# Ultroid - UserBot
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# .tweet made for ultroid
+
+
+"""
+✘ Commands Available -
+
+• `{i}tweet`
+    make twitter posts.
+
+"""
+
+from . import *
+
+@ultroid_cmd(pattern="tweet ?(.*)")
+async def tweet(e):
+    wai = await eor(e,"processing...")
+    text = e.pattern_match.group(1)
+    try:
+        results = await ultroid_bot.inline_query(
+            "twitterstatusbot",text
+        )
+        await results[0].click(
+            e.chat_id,
+            silent=True,
+            hide_via=True,
+        )
+        await wai.delete()
+    except ChatSendInlineForbiddenError:
+        await wai.edit("`Boss ! I cant use inline things here...`")
+    except ChatSendStickersForbiddenError:
+        await wai.edit("Sorry boss, I can't send Sticker Here !!")
+        
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
