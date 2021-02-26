25161c0c5c311c37cdeb3f0498ccb2d7e05ec451
parth0207 <74072249+parth0207@users.noreply.github.com>
2021-02-26 22:31:05 +0530

    Add files via upload

---
+++ prankpromote.py
@@ -0,0 +1,13 @@
+#Written By @senku_ishigamiii and inspiried from DarkCobra Wala Plugin
+import asyncio
+
+@ultroid_cmd(pattern="pprank")
+async def pprank(ult):
+    if not ult.text[0].isalpha() and ult.text[0] not in ("/", "#", "@", "!"):
+        await ult.edit("**PROMOTING USER..***")
+        await asyncio.sleep(1)
+        await ult.edit("**PROMOTING USER...**")
+        await asyncio.sleep(1)
+        await ult.edit("**GIVING RIGHTS**")
+        await asyncio.sleep(1)
+        await ult.edit("**PROMOTED USER SUCCESSFULLY**")
