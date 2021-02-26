2d97b6c3afd634539b0d805a7c96dbb5281a47ca
parth0207 <74072249+parth0207@users.noreply.github.com>
2021-02-26 22:36:34 +0530

    Delete okboss.py

--- okboss.py
+++
@@ -1,8 +0,0 @@
-@ultroid_cmd(pattern="okboss")
-async def okboss(ult):
-	await ult.edit("OK FUCKER")
-	await ult.edit("OK BITCH")
-	await ult.edit("OK GAY")
-	await ult.edit("OK SIR")
-	await ult.edit("OK MADAM")
-	await ult.edit("OK BOSS! 😇")
