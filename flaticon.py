8c4c193cafbd56a14951e2b7170211535b4f3754
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-03-07 19:38:40 +0530

    Create flaticon.py

---
+++ flaticon.py
@@ -0,0 +1,43 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+
+"""
+✘ Commands Available
+• `{i}flaticon <query>`
+
+    Icon search from flaticon.com and uploading as sticker.
+    
+"""
+
+import os
+import random
+import urllib
+import requests
+from bs4 import BeautifulSoup as bs
+from . import *
+
+
+@ultroid_cmd(pattern="flaticon ?(.*)")
+async def www(e):
+    a = e.pattern_match.group(1)
+    if not a:
+        return await eor(e,"Give some Text to Get Icon from Flaticon.com")
+    tt = await eor(e,"Processing...")
+    query  = a.replace(" ","%20")
+    link = F"https://www.flaticon.com/search?word={query}"
+    ge = requests.get(link).content
+    cl = bs(ge,"lxml",from_encoding="utf-8")
+    results = cl.find_all("img",src="https://media.flaticon.com/dist/min/img/loader.gif")
+    dome = results[random.randrange(0,len(results) - 1)]["data-src"]
+    urllib.request.urlretrieve(dome,"sticker.webp")
+    await ultroid_bot.send_file(e.chat.id,"sticker.webp")
+    os.remove("sticker.webp")
+    await tt.delete()
+
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
