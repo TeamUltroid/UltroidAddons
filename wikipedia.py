1dc48ced9105b441b846037476f1b487b83c2158
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-03-09 12:08:16 +0530

    Update wikipedia.py

--- wikipedia.py
+++ wikipedia.py
@@ -1,7 +1,5 @@
 # Ultroid Userbot
-
-# made by member of TeamUltroid
-
+#
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
