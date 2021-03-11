fec48dc0d7ef81ab260247d1ca85a64ab0f9c17b
ArnabXD <arnabxd@pm.me>
2021-03-11 15:00:39 +0530

    handle no result found

--- flaticon.py
+++ flaticon.py
@@ -28,16 +28,19 @@ async def www(e):
     if not a:
         return await eor(e,"Give some Text to Get Icon from Flaticon.com")
     tt = await eor(e,"`Processing...`")
-    query  = a.replace(" ","%20")
-    link = f"https://www.flaticon.com/search?word={query}"
-    ge = requests.get(link).content
-    cl = bs(ge,"lxml",from_encoding="utf-8")
-    results = cl.find_all("img",src="https://media.flaticon.com/dist/min/img/loader.gif")
-    dome = results[random.randrange(0,len(results) - 1)]["data-src"]
-    urllib.request.urlretrieve(dome,"sticker.webp")
-    await ultroid_bot.send_file(e.chat.id,"sticker.webp")
-    os.remove("sticker.webp")
-    await tt.delete()
+    query = a.replace(" ", "%20")
+    try:
+        link = f"https://www.flaticon.com/search?word={query}"
+        ge = requests.get(link).content
+        cl = bs(ge, "lxml", from_encoding="utf-8")
+        results = cl.find_all("img", src="https://media.flaticon.com/dist/min/img/loader.gif")
+        dome = results[random.randrange(0, len(results) - 1)]["data-src"]
+        urllib.request.urlretrieve(dome, "sticker.webp")
+        await ultroid_bot.send_file(e.chat.id, "sticker.webp")
+        os.remove("sticker.webp")
+        await tt.delete()
+    except:
+        await tt.edit("`No Results Found`")
 
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
