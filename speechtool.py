0553071c37233b30efc5aa6e6f439307fc7861d6
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-03-13 11:38:26 +0530

    Create speechtool.py

---
+++ speechtool.py
@@ -0,0 +1,91 @@
+#
+# Ultroid - UserBot
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+#
+# Ported from Telebot
+
+
+""" 
+✘ Commands Available -
+
+• {i}tts LanguageCode <reply to a message>
+• {i}tts LangaugeCode | text to speak
+
+"""
+
+
+import asyncio
+import os
+import subprocess
+from datetime import datetime
+from . import *
+from gtts import gTTS
+
+
+@ultroid_cmd(
+    pattern="tts ?(.*)",
+)
+async def _(event):
+    input_str = event.pattern_match.group(1)
+    start = datetime.now()
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await eor(event, "Invalid Syntax. Module stopping.")
+        return
+    text = text.strip()
+    lan = lan.strip()
+    if not os.path.isdir('downloads/'):
+        os.makedirs('downloads/')
+    required_file_name =  "downloads/voice.ogg"
+    try:
+        tts = gTTS(text, lang=lan)
+        tts.save(required_file_name)
+        command_to_execute = [
+            "ffmpeg",
+            "-i",
+            required_file_name,
+            "-map",
+            "0:a",
+            "-codec:a",
+            "libopus",
+            "-b:a",
+            "100k",
+            "-vbr",
+            "on",
+            required_file_name + ".opus",
+        ]
+        try:
+            t_response = subprocess.check_output(
+                command_to_execute, stderr=subprocess.STDOUT
+            )
+        except (subprocess.CalledProcessError, NameError, FileNotFoundError) as exc:
+            await eor(event, str(exc))
+        else:
+            os.remove(required_file_name)
+            required_file_name = required_file_name + ".opus"
+        end = datetime.now()
+        ms = (end - start).seconds
+        await borg.send_file(
+            event.chat_id,
+            required_file_name,
+            # caption="Processed {} ({}) in {} seconds!".format(text[0:97], lan, ms),
+            reply_to=event.message.reply_to_msg_id,
+            allow_cache=False,
+            voice_note=True,
+        )
+        os.remove(required_file_name)
+        await eor(event, "Processed {} ({}) in {} seconds!".format(text[0:97], lan, ms))
+        await asyncio.sleep(5)
+        await event.delete()
+    except Exception as e:
+        await eor(event, str(e))
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
