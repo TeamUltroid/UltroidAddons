11c056a484694a6becffad1810ea7557a4c380ad
Error <error@notavailable.live>
2021-03-29 18:53:22 +0530

    Covid-19 Information about a country.

---
+++ covid.py
@@ -0,0 +1,34 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+"""
+✘ Commands Available
+• `{i}covid country name`
+    Gets the Covid-19 Status of a given Country.
+"""
+
+from covid import Covid
+
+from . import *
+
+@ultroid_cmd(pattern="covid")
+async def coronish(event):
+  covid = Covid()
+  text = event.text
+  okie = text.split(" ", maxsplit=1)
+  country = okie[1]
+  try:
+    cases = covid.get_status_by_country_name((country).lower())
+    act = cases["active"]
+    conf = cases["confirmed"]
+    dec = cases["deaths"]
+    rec = cases["recovered"]
+    await eor(event, f"**Country:** **{country.capitalize()}**\n**Active:** {act}\n**Confirmed:** {conf}\n**Recovered:** {rec}\n**Deceased:** {dec}")
+  except ValueError:
+    await eor(event, f"It seems that Country {country} is invalid!")
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
