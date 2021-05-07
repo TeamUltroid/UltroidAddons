a21e67b00320998d11028cdb9c1b100e34abc22e
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-07 22:08:11 +0530

    Update song.py

--- song.py
+++ song.py
@@ -30,7 +30,7 @@ import json
 import os
 import random
 import time
-
+from lyrics_extractor.lyrics import LyricScraperException as LyError
 from lyrics_extractor import SongLyrics as sl
 from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
 from telethon.tl.functions.messages import ImportChatInviteRequest
@@ -236,7 +236,10 @@ async def original(event):
     if dc == 3:
         danish = "AIzaSyDdOKnwnPwVIQ_lbH5sYE4FoXjAKIQV0DQ"
     extract_lyrics = sl(f"{danish}", "15b9fb6193efd5d90")
-    sh1vm = extract_lyrics.get_lyrics(f"{noob}")
+    try:
+        sh1vm = extract_lyrics.get_lyrics(f"{noob}")
+    except LyError:
+        return await eod(event, 'No Results Found')
     a7ul = sh1vm["lyrics"]
     await ultroid_bot.send_message(event.chat_id, a7ul, reply_to=event.reply_to_msg_id)
     await ab.delete()
