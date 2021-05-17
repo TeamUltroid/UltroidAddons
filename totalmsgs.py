52ea68a0e79e4769094c96a21c3b463a3e0e8120
Mustafa Shaikh <smustfa147@gmail.com>
2021-05-17 14:55:18 +0530

    Totalmsgs plugin (#56)

---
+++ totalmsgs.py
@@ -0,0 +1,23 @@
+# credit https://t.me/I_m_FlaSh
+
+"""
+✘ Commands Available -
+• `{i}totalmsgs`
+    Returns your total msg count in current chat
+    
+• `{i}totalmsgs [username]`
+    Returns total msg count of user in current chat
+"""
+
+from . import *
+
+@ultroid_cmd(pattern="totalmsgs ?(.*)")
+async def _(e):
+  u = e.pattern_match.group(1)
+  if not u:
+    u = "me"
+  a=await ultroid_bot.get_messages(e.chat_id, 0, from_user=u)
+  await eor(e, f'Total msgs of {u} here = {a.total}')
+  
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})  
