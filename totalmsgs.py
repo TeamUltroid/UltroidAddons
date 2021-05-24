8c4898f78ea365cfdc899ffd95f9abae17c84e45
Danish <danish@ultroid.tech>
2021-05-24 11:57:02 +0530

    Update totalmsgs.py

--- totalmsgs.py
+++ totalmsgs.py
@@ -13,11 +13,15 @@ from . import *
 
 @ultroid_cmd(pattern="totalmsgs ?(.*)")
 async def _(e):
+  k = await e.get_reply_message()
+  if k :
+      a = await ultroid_bot.get_messages(e.chat_id, 0, from_user=k.sender_id)
+      return await eor(e, f"Total msgs of {u} here = {a.total}")
   u = e.pattern_match.group(1)
   if not u:
     u = "me"
-  a=await ultroid_bot.get_messages(e.chat_id, 0, from_user=u)
-  await eor(e, f'Total msgs of {u} here = {a.total}')
+  a = await ultroid_bot.get_messages(e.chat_id, 0, from_user=u)
+  await eor(e, f'Total msgs of {u} here = {a.total}")
   
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})  
