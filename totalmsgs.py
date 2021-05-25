71fd3dc9ce521514675d13ba3fe88c28bc306ff4
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-25 16:27:31 +0530

    reply support

--- totalmsgs.py
+++ totalmsgs.py
@@ -5,23 +5,25 @@
 • `{i}totalmsgs`
     Returns your total msg count in current chat
     
-• `{i}totalmsgs [username]`
+• `{i}totalmsgs [username]/<reply>`
     Returns total msg count of user in current chat
 """
 
 from . import *
 
+
 @ultroid_cmd(pattern="totalmsgs ?(.*)")
 async def _(e):
-  k = await e.get_reply_message()
-  if k :
-      a = await ultroid_bot.get_messages(e.chat_id, 0, from_user=k.sender_id)
-      return await eor(e, f"Total msgs of {u} here = {a.total}")
-  u = e.pattern_match.group(1)
-  if not u:
-    u = "me"
-  a = await ultroid_bot.get_messages(e.chat_id, 0, from_user=u)
-  await eor(e, f"Total msgs of {u} here = {a.total}")
-  
+  match = e.pattern_match.group(1)
+  if match:
+    user = match
+  elif e.is_reply:
+    user = (await e.get_reply_message()).sender_id
+  else:
+    user = 'me'
+  a = await ultroid_bot.get_messages(e.chat_id, 0, from_user=user)
+  user = await ultroid_bot.get_entity(user)
+  await eor(e, f"Total msgs of `{user.first_name}` here = {a.total}")
+
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})  
