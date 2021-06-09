4c7078f997db9867bdceb3001245ba65a1aa4730
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-06-09 18:20:23 +0530

    Update and rename how_google.py to googleutility.py

--- googleutility.py
+++ googleutility.py
@@ -11,10 +11,14 @@
 •`{i}htg <text>`
    How To Google.
    Some peoples don't know how to google so help them 🙃🙂.
+
+•`{i}doodle`
+   Get Today's Google Doodle.
 """
 
 
 import requests
+from bs4 import BeautifulSoup as bs
 
 from . import *
 
@@ -32,3 +36,16 @@ async def _(e):
         await eor(e, "[{}]({})\n`Thank me Later 🙃` ".format(text, response.rstrip()))
     else:
         await eod(e, "`something is wrong. please try again later.`")
+
+
+@ultroid_cmd(pattern="doodle$")
+async def gdoodle(event):
+    cont = requests.get("https://google.com").content
+    try:
+        bst = bs(cont, "html.parser", from_encoding="utf-8")
+        m = bst.find_all("meta", {"property":"twitter:image"})
+        await event.reply(caption="Here is Today's Google Doodle", file=m[0]["content"])
+        if event.out:
+            await event.delete()
+    except:
+        await eor(event, "`FAILED to Get Google Doodle.`")
