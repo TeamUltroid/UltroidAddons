dcf04a8098a3340d34e855ea8ff410a085537385
Aditya <xditya@ultroid.tech>
2021-06-14 15:29:23 +0530

    bye

--- animedb.py
+++ animedb.py
@@ -5,9 +5,6 @@
 Search animes and manga from anilist.co using @animedb_bot
 
 ✘ Commands Available
-• `{i}anime <keyword>`
-    To get anime info
-
 • `{i}manga <keyword>`
     To get manga info
 """
@@ -17,30 +14,6 @@ from telethon.errors import ChatSendInlineForbiddenError
 from . import *
 
 INLOCK = "`Seems like inline messages aren't allowed here`"
-
-
-@ultroid_cmd(
-    pattern="anime ?(.*)",
-)
-async def anime(ult):
-    msg = await eor(ult, "`Searching ...`")
-    keyword = ult.pattern_match.group(1)
-    if keyword is None:
-        return await msg.edit("`Provide a Keyword to search`")
-    try:
-        animes = await ultroid_bot.inline_query("animedb_bot", keyword)
-        await animes[0].click(
-            ult.chat_id,
-            reply_to=ult.reply_to_msg_id,
-            silent=True if ult.is_reply else False,
-            hide_via=True,
-        )
-        return await msg.delete()
-    except ChatSendInlineForbiddenError:
-        return await msg.edit(INLOCK)
-    except Exception:
-        return await msg.edit("`No Results Found ...`")
-
 
 @ultroid_cmd(
     pattern="manga ?(.*)",
