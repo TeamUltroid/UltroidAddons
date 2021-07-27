a52ba32642676998cc7e3b35b62d2835ab9ec598
Devesh Pal <newdev0@outlook.com>
2021-07-28 00:08:43 +0530

    auto: Format the code

--- wreplace.py
+++ wreplace.py
@@ -1,26 +1,40 @@
 #credits to @Harpia-Vieillot
 #For @TeamUltroid
-'''
+
+"""
 ✘ Commands Available
+
 • `{i}wreplace <count> <old word>;<new word>`
-    Note : Don't use brackets\n\nEx. : {i}replace 10;Hi;Hello\n\nUse: It replaces a perticular word by new word (only in your msgs.) In many msgs at a time
-'''
+    Note : Don't use brackets
+
+    Ex. :
+    {i}replace 10;Hi;Hello
+
+   Use: It replaces a perticular word by new word (only in your msgs.) In many msgs at a time
+"""
+
+import asyncio
 from . import *
-import time
+
+
 @ultroid_cmd(pattern="wreplace")
 async def harpia(e):
-  sed = str(e.text[10:])
-  lst = sed.split(" ", 1)
-  lmt = int(lst[0]) + 1
-  s = lst[1]
-  pist = s.split(";")
-  async for x in bot.iter_messages(e.chat_id, limit= lmt, from_user="me"):
-  	msg = x.text
-  	if pist[0] in msg:
-  	 m = msg.replace(pist[0], pist[1])
-  	 await x.edit(m)
-  await e.edit("Finished...")
-  time.sleep(5)
-  await e.delete()
+    try:
+        sed = str(e.text[10:])
+        lst = sed.split(" ", 1)
+        lmt = int(lst[0]) + 1
+        s = lst[1]
+        pist = s.split(";")
+        pist[1]
+    except IndexError:
+        return eod(e, f"Check Example : `{HNDLR}help {wreplace}`")
+    async for x in e.client.iter_messages(e.chat_id, limit=lmt, from_user="me"):
+  	    msg = x.text
+  	    if pist[0] in msg:
+  	        m = msg.replace(pist[0], pist[1])
+  	        await x.edit(m)
+            await asyncio.sleep(1)
+    await eod(e, "Finished...")
+
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
