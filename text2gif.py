b22b1801ff5ea18e0b88a54d0c76709aaff4e59c
Devesh Pal <newdev0@outlook.com>
2021-07-30 12:32:35 +0530

    auto: PreCommit

--- text2gif.py
+++ text2gif.py
@@ -5,65 +5,59 @@
 """
 ✘ **Makes Fancy Gif from your Words!**
 
-✘ ** text2gif -> t2g **
- eg : `{i}t2g <some_text>`
+✘ `{i}t2g <some_text>`
+    `Convert Text to Gif...`
 """
 
 import emoji
-import re
-
-from telethon import functions, types
-from random import randint as rnd
+import re, os
+import random
+from telethon.utils import get_input_document
 
 from . import *
 
-
 chat = "text2gifBot"
+
 def remove_emoji(string):
     return emoji.get_emoji_regexp().sub(u'', string)
 
 
-@ultroid_cmd(pattern="t2g ?(.*)")
+@ultroid_cmd(pattern="t2g")
 async def t2g(e):
-    if e.fwd_from:
-        return
     eris = await eor(e, "`...`")
-    reply_to = e.reply_to_msg_id if e.is_reply else None
     input_args = e.pattern_match.group(1) 
     if not input_args:
         input_args = "No Text was Given :(("
     args = remove_emoji(input_args)
     try:
-        t2g = await ultroid.inline_query(chat, args)
-        to_me = await t2g[rnd(0, 27)].click("me")
-        done = await ultroid.send_file(
+        t2g = await e.client.inline_query(chat, args)
+        doc = t2g[random.randrange(0, len(t2g) -1)]
+        try:
+            file = await doc.download_media()
+            done = await e.client.send_file(
             e.chat_id,
-            to_me,
-            reply_to=reply_to,
-        )
+            file = file,
+            reply_to=e.reply_to_msg_id
+            )
+            os.remove(file)
+        except AttributeError:
+            done = await doc.click(
+                e.chat_id,
+                reply_to=e.reply_to_msg_id)
         await eris.delete()
-        await to_me.delete()
     except Exception as fn:
-        return await eod(eris, f"`{fn}`")
+        return await eod(eris, f"**ERROR** : `{fn}`")
     await cleargif(done)
-
 
 
 async def cleargif(gif_):
     try:
         await ultroid(
             functions.messages.SaveGifRequest(
-                id=types.InputDocument(
-                    id=gif_.media.document.id,
-                    access_hash=gif_.media.document.access_hash,
-                    file_reference=gif_.media.document.file_reference,
-                ), 
+                id=get_input_document(gif_),
                 unsave=True,
             )
         )
-    except Exception:
-        pass
-
-
-HELP.update({f"{_name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
+    except Exception as E:
+        LOGS.info(E)
 
