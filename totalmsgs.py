b220f25a29e52aedf840751fbecd4f09db321628
Devesh Pal <newdev0@outlook.com>
2021-09-03 14:37:57 +0530

    Remove empty lines

--- totalmsgs.py
+++ totalmsgs.py
@@ -25,5 +25,3 @@ async def _(e):
     a = await e.client.get_messages(e.chat_id, 0, from_user=user)
     user = await e.client.get_entity(user)
     await eor(e, f"Total msgs of `{get_display_name(user)}` here = {a.total}")
-
-
