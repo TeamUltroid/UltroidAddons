6358858c675510bc0054a3697c3d3df43325096c
Danish <danish@ultroid.tech>
2021-09-15 23:48:15 +0530

    Update limited.py

--- limited.py
+++ limited.py
@@ -15,22 +15,19 @@ from . import *
 
 @ultroid_cmd(pattern="limited$")
 async def _(e):
-   msg= await eor(e, "checking if you are limited or not...")
-
+   msg = await eor(e, "checking if you are limited or not...")
    if e.reply_to_msg_id:
-     reply= await e.get_reply_message()
-     lel= await bot.get_entity(reply.sender_id)
-     x= lel.restriction_reason
-     if not x==None:
-       await msg.edit(x)
+     reply = await e.get_reply_message()
+     lel = await ultroid_bot.get_entity(reply.sender_id)
+     x = lel.restriction_reason
+     if x:
+       await msg.edit(f"`{x}`")
      else: 
-       await msg.edit(f'Good news, no limits are currently applied to {reply.sender.first_name}'s account. {reply.sender.first_name} is free as a bird!')
-   else:
-
-     pro= await bot.get_entity('me')
-     xx= pro.restriction_reason
-     if not xx==None:
-       await msg.edit(xx)
+       await msg.edit(f"Good news, no limits are currently applied to {reply.sender.first_name}'s account. {reply.sender.first_name} is free as a bird!")
+   else: 
+     xx = ultroid_bot.me.restriction_reason
+     if xx:
+       await msg.edit(f"`{xx}`")
      else:
-       await msg.edit('Good news, no limits are currently applied to your account. You’re free as a bird!')
+       await msg.edit("Good news, no limits are currently applied to your account. You’re free as a bird!")
      
