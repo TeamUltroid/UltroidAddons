def97efcd5a63a3611c10458ec2ffe8ea796c15f
nub-af <88770407+nub-af@users.noreply.github.com>
2021-09-15 23:38:36 +0530

    Update limited.py
    Now you can check anyone's account is limited or not👀

--- limited.py
+++ limited.py
@@ -1,33 +1,36 @@
-# inspired from bin.py which was made by @danish_00
-# written by @senku_ishigamiii/@Uzumaki_Naruto_XD
+# updated by @nub_a_f
+# previously @senku_ishigamiii/ @Uzumaki_Naruto_XD
+# that can only check client/owner's limitations
+# but now can check anyone's limitations
 
 """
 ✘ Commands Available -
 
 • `{i}limited`
-   Check you are limited or not !
+   Check you/anyone are limited or not !
 
 """
 
-from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-
 from . import *
 
-
 @ultroid_cmd(pattern="limited$")
-async def demn(ult):
-    chat = "@SpamBot"
-    msg = await eor(ult, "Checking If You Are Limited...")
-    async with ult.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=178220800)
-            )
-            await conv.send_message("/start")
-            response = await response
-            await ult.client.send_read_acknowledge(chat)
-        except YouBlockedUserError:
-            await msg.edit("Boss! Please Unblock @SpamBot ")
-            return
-        await msg.edit(f"~ {response.message.message}")
+async def _(e):
+   msg= await eor(e, "checking if you are limited or not...")
+
+   if e.reply_to_msg_id:
+     reply= await e.get_reply_message()
+     lel= await bot.get_entity(reply.sender_id)
+     x= lel.restriction_reason
+     if not x==None:
+       await msg.edit(x)
+     else: 
+       await msg.edit(f'Good news, no limits are currently applied to {reply.sender.first_name}'s account. {reply.sender.first_name} is free as a bird!')
+   else:
+
+     pro= await bot.get_entity('me')
+     xx= pro.restriction_reason
+     if not xx==None:
+       await msg.edit(xx)
+     else:
+       await msg.edit('Good news, no limits are currently applied to your account. You’re free as a bird!')
+     
