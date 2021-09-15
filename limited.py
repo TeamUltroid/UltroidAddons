6975bf4a1e018d91f87e9135ad66078691a714b5
Devesh Pal <newdev0@outlook.com>
2021-09-15 23:55:23 +0530

    Better Codes and Query support
    • Rip that trespassers

--- limited.py
+++ limited.py
@@ -7,27 +7,26 @@
 ✘ Commands Available -
 
 • `{i}limited`
-   Check you/anyone are limited or not !
+   Check restrictions on someone/self account!
 
 """
 
 from . import *
 
-@ultroid_cmd(pattern="limited$")
+@ultroid_cmd(pattern="limited ?(.*)")
 async def _(e):
-   msg = await eor(e, "checking if you are limited or not...")
-   if e.reply_to_msg_id:
-     reply = await e.get_reply_message()
-     lel = await ultroid_bot.get_entity(reply.sender_id)
-     x = lel.restriction_reason
-     if x:
-       await msg.edit(f"`{x}`")
-     else: 
-       await msg.edit(f"Good news, no limits are currently applied to {reply.sender.first_name}'s account. {reply.sender.first_name} is free as a bird!")
-   else: 
-     xx = ultroid_bot.me.restriction_reason
-     if xx:
-       await msg.edit(f"`{xx}`")
-     else:
-       await msg.edit("Good news, no limits are currently applied to your account. You’re free as a bird!")
-     
+   match = e.pattern_match.group(1)
+   msg = await eor(e, "checking if account is limited or not...")
+   if match:
+       lel = match
+   elif e.reply_to_msg_id:
+       reply = await e.get_reply_message()
+       lel = reply.sender_id
+   else:
+       lel = "me"
+   pro = await e.client.get_entity(lel)
+   rest = pro.restriction_reason
+   if rest :
+       xx = f"Restriction on {get_display_name(pro)}\n".join(f"\n• `{a}`" for a in rest)
+       return await msg.edit(xx)
+   return await msg.edit("`Good news, no limits are currently applied to account. You’re free as a bird!`")
