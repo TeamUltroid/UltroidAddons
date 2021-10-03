fbe0cecee966364639e960d78c3807de985f37b7
New-dev0 <newdev0@outlook.com>
2021-10-03 12:11:50 +0000

    commit from tg

--- random.py
+++ random.py
@@ -22,31 +22,35 @@ from . import async_searcher, ultroid_bot, eor, HNDLR
 # These Api's are Collected From
 # ---- https://github.com/public-apis/public-apis
 
-API_LIST = {"cat":"https://aws.random.cat/meow", "dog":"https://random.dog/woof.json",
-"duck":"https://random-d.uk/api/random","fox":"https://randomfox.ca/floof/",
-"funfact":"https://asli-fun-fact-api.herokuapp.com/",
+API_LIST = {
+    "cat": "https://aws.random.cat/meow",
+    "dog": "https://random.dog/woof.json",
+    "duck": "https://random-d.uk/api/random",
+    "fox": "https://randomfox.ca/floof/",
+    "funfact": "https://asli-fun-fact-api.herokuapp.com/",
 }
+
 
 @ultroid_cmd(pattern="random ?(.*)")
 async def random_magic(event):
-   match = event.pattern_match.group(1)
-   if not (match and match in API_LIST.keys()):
-       return await eor(event, f"`Input Missing/Wrong..`\n`{HNDLR}help random`")
-   text, file = None, None
-   try:
-       req = await async_searcher(API_LIST[match], re_json=True)
-   except Exception as er:
-       return await eor(event, str(er))
-   if match == "cat":
-       file = req["file"]
-   elif match in ["dog", "duck"]:
-       file = req["url"]
-   elif match == "fox":
-       file = req["image"]
-   elif match == "funfact":
-       text = req["data"]["fact"]
-   if text and not file:
-       return await eor(event, text)
-   else:
-       await event.reply(text, file=file)
-       await event.delete()
+    match = event.pattern_match.group(1)
+    if not (match and match in API_LIST.keys()):
+        return await eor(event, f"`Input Missing/Wrong..`\n`{HNDLR}help random`")
+    text, file = None, None
+    try:
+        req = await async_searcher(API_LIST[match], re_json=True)
+    except Exception as er:
+        return await eor(event, str(er))
+    if match == "cat":
+        file = req["file"]
+    elif match in ["dog", "duck"]:
+        file = req["url"]
+    elif match == "fox":
+        file = req["image"]
+    elif match == "funfact":
+        text = req["data"]["fact"]
+    if text and not file:
+        return await eor(event, text)
+    else:
+        await event.reply(text, file=file)
+        await event.delete()
