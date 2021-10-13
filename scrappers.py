bea45226cd5fd6885232ff4ee946e480c5dfe6bd
Devesh Pal <newdev0@outlook.com>
2021-10-13 19:43:45 +0530

    Update scrappers.py

--- scrappers.py
+++ scrappers.py
@@ -8,3 +8,19 @@
 """
 
 # TODO : Complete Plugin 😴
+
+from . import *
+import requests as r
+from bs4 import BeautifulSoup as bs
+
+@ultroid_cmd(pattern="shutter ?(.*)")
+async def scrap_shutter(event):
+  match = event.pattern_match.group(1)
+  if not match:
+    return await eor(event, "`Give Some text to Search for..`")
+  uri = "https://www.shutterstock.com/search/" + match.replace(" ", "+")
+  reqs = r.get(uri).content
+  bbb = bs(reqs, "html.parser", from_encoding="utf-8")
+  ml = bbb.find_all("img", src=re.compile("https://image.shutterstock.com/image-photo"))
+  LOGS.info(ml)
+    
