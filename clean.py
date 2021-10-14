f56b9377796b479102b53ab85c3f8ab2000cfe7b
hurR <hurr@gmail.com>
2021-10-14 04:02:12 +0000

    k

--- clean.py
+++
@@ -1,9 +0,0 @@
-from . import *
-
-
-@asst.on(events.ChatAction(func=lambda x: x.user_joined, chats=["ultroidpluginschat"]))
-async def hhh(e):
-    m = await e.get_sender()
-    a = await e.client.kick_participant(e.chat_id, m.id)
-    await e.delete()
-    await a.delete()
--- safes.py
+++
@@ -1,5 +0,0 @@
-from . import *
-
-ultroid_bot.delete_dialog = None
-asst.delete_dialog = None
-ultroid_bot.kick_participant = None
