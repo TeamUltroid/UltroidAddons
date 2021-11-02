943be669345a8337a8feca64ffcab41660a0fbbe
Devesh Pal <newdev0@outlook.com>
2021-11-02 17:29:32 +0530

    Hurr

---
+++ horoscope.py
@@ -0,0 +1,30 @@
+# Ultroid Userbot
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+"""
+✘ Commands Available
+
+• `horo sign`
+  `Get Horoscope for Today..`
+
+"""
+
+from . import *
+
+
+@ultroid_cmd(pattern="horo ?(.*)")
+async def hhhh(eve):
+    match = event.pattern_match.group(1)
+    if not match:
+        return await eor(eve, "`Provide your sign to get Your Horoscope..`")
+    data = await async_searcher("https://aztro.sameerkumar.website/?sign={match}&day=today", post=True, re_json=True)
+    if data.get("message"):
+        return await eor(eve, f"**ERROR :** `{data.get('message').split('.')[0]}`")
+    msg = ""
+    for key in data.keys():
+        _key = key[0].upper() + key[1:]
+        msg += f"• **{_key.replace('_', ' ')}** : `{data['key']}`"
+    await eor(eve, msg)
