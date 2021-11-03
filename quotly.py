f1e3781c012c8cc380233900d83c4afa524a13de
Devesh Pal <newdev0@outlook.com>
2021-11-03 14:19:13 +0530

    Update quotly.py

--- quotly.py
+++ quotly.py
@@ -8,9 +8,6 @@
 """
 ✘ Commands Available -
 
-• `{i}quotly | {i}qbot <colour name/code><replying a message>`
-    send stickers to current chat with QuotlyBot.
-
 • `{i}q <reply>`
     Make sticker quote without QuotlyBot
 """
@@ -21,37 +18,6 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from . import *
-
-ERR = "`Can you kindly disable your forward privacy settings for good?`"
-
-
-@ultroid_cmd(pattern="(quotly|qbot) ?(.*)")
-async def _(event):
-    if not event.reply_to_msg_id:
-        return await eor(event, "```Reply to any user message.```")
-    reply_message = await event.get_reply_message()
-    chat = "@QuotLyBot"
-    reply_message.sender
-    ac = await eor(event, "```Making a Quote```")
-    col = event.pattern_match.group(2)
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1031952739)
-            )
-            er = await event.client.forward_messages(chat, reply_message)
-            if not len(col) == 0:  # Bad way
-                await asyncio.sleep(3)
-                await er.reply(f"/q {col}")
-            response = await response
-            await ultroid_bot.send_read_acknowledge(chat)
-        except YouBlockedUserError:
-            return await event.reply("```Please unblock @QuotLyBot and try again```")
-        if response.text.startswith("Hi!"):
-            await eor(event, ERR)
-        else:
-            await ac.delete()
-            await event.client.send_message(event.chat_id, response.message)
 
 
 # Oringinal Source from Nicegrill: https://github.com/erenmetesar/NiceGrill/
