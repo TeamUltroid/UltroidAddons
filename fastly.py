ddb7e4436ff91de1200b1a1bc3a757abc432c07f
Devesh Pal <newdev0@outlook.com>
2022-01-12 14:50:41 +0530

    Scam

--- fastly.py
+++ fastly.py
@@ -8,38 +8,53 @@
 """
 Fasly Bot Cheat.
 
-Use `{i}setdb FASTLY True` to turn this on.
-The bot would auto reply first to the messages by @FastlyWriteBot and @FastlyWriteCloneBot.
+• `{i}fastly` - On/Off command. 
+
+• Also Required : `OCR_API`
+The bot will try to auto reply first to the messages by @FastlyWriteBot and @FastlyWriteCloneBot.
 """
 
 from . import async_searcher
 from telegraph import upload_file
 from telethon import events
-from . import udB
+from . import udB, LOGS, ultroid_bot, ultroid_cmd
 
 base_url = "https://api.ocr.space/parse/imageurl?apikey={api}&url={tgraph}"
 
 
-@ultroid_bot.on(events.NewMessage(incoming=True, from_users=[1806208310, 1983714367]))
 async def fastly_bot(event):
-    if udB.get("FASTLY") != "True":
+    if not udB.get_key("FASTLY"):
         return
-    api = udB.get("OCR_API")
-    if not api:
+    api = udB.get_key("OCR_API")
+    if not (api and event.photo):
         return
-    if not event.media and not event.media.photo:
-      return
     med = await event.download_media()
     upload = upload_file(med)
     link = "https://telegra.ph" + upload[0]
     out = await async_searcher(base_url.format(api=api, tgraph=link), re_json=True)
     try:
         txt = out["ParsedResults"][0]["ParsedText"]
-    except:
+    except (KeyError, IndexError):
         return
     txt = txt.replace("By@FastlyWriteBot", "").replace("\n", "").replace("\r", "")
     if txt:
         try:
             await event.reply(txt)
-        except:
-            return
+        except Exception as er:
+            LOGS.exception(er)
+
+
+@ultroid_cmd(pattern="fastly$")
+async def fastOnOff(event):
+    xx = await event.eor("`...`")
+    get_ = udB.get_key("FASTLY")
+    if not get_:
+        udB.set_key("FASTLY", True)
+        ultroid_bot.add_handler(fastly_bot, events.NewMessage(incoming=True, from_users=[1806208310, 1983714367]))
+        return await xx.edit("`Auto Fastly Response Activated`")
+    udB.del_key("FASTLY")
+    await xx.edit("`Fastly Stopped!`")
+
+
+if udB.get_key("FASTLY"):
+    ultroid_bot.add_handler(fastly_bot, events.NewMessage(incoming=True, from_users=[1806208310, 1983714367]))
