c08807861fd063b6f284ef22d4ce625ca6c039f0
Devesh Pal <newdev0@outlook.com>
2022-03-03 10:38:04 +0530

    srdtyuiop[

--- imgwhale.py
+++ imgwhale.py
@@ -12,11 +12,26 @@
 • `{i}imgwhale <Optional<apiKey>>`
     Upload image to https://ImgWhale.xyz !
 """
+from . import *
 
-import requests
 
-@ultroid_cmd(pattern="imgwhale ?(.*)")
+@ultroid_cmd(pattern="imgwhale")
 async def imgwhale(event):
     msg = await event.eor("`Processing...`")
-    api_key = event.pattern_match.group(1)
-    # If api_key exists upload with that else do with normal method.
+    reply = await event.get_reply_message()
+    if not reply:
+        return await msg.edit("`Reply to Image...`")
+    if reply.photo:
+        file = await reply.download_media()
+    elif reply.document and reply.document.thumbs:
+        file = await reply.download_media(thumb=-1)
+    else:
+        return await msg.edit("`Reply to Image...`")
+    api_key = udB.get_key("IMGWHALE_KEY")
+    json = {}
+    if api_key:
+        json.update({"key":api_key})
+    post = await async_searcher("https://imgwhale.xyz/new", post=True, data={'image': open(file, 'rb')} ,json=json, re_json=True)
+    if post.get("error"):
+        return await msg.edit(post["message"])
+    await msg.edit(f"Successfully Uploaded to [ImhWhale](https://imgwhale.xyz/{post['fileId']})")
