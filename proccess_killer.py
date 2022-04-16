27efb6fb052c4553982cf954c96caf2e9133a88a
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-04-16 18:11:46 +0530

    Fixed

--- proccess_killer.py
+++ proccess_killer.py
@@ -24,10 +24,10 @@ async def _(event):
     pname = event.pattern_match.group(1)
     if not pname:
         return await event.eor("`Give Any Proccess Name To Kill it`")
-    await event.eor("`Killing The Proccess Named` **{pname}**")
+    await event.eor(f"`Killing The Proccess Named` **{pname}**")
     for p in psutil.process_iter():
         pnames = p.name()
         if pnames == pname:
             os.kill(p.pid, signal.SIGKILL)
-            return await event.edit("`Successfully Killed The Proccess Named` **{pname}**")
+            return await event.edit(f"`Successfully Killed The Proccess Named` **{pname}**")
     return await event.edit("`Given Proccess Not Found`")
