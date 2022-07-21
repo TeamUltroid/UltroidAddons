91652f8c9f36d6229ed68de315c2ccfb1f681700
Devesh Pal <newdev0@outlook.com>
2022-07-21 19:43:36 +0530

    Update koo.py

--- inline/koo.py
+++ inline/koo.py
@@ -6,6 +6,8 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
+from telethon.tl.types import InputWebDocument as wb
+from telethon.tl import Button
 from . import in_pattern, InlinePlugin, async_searcher
 
 _koo_ = {}
