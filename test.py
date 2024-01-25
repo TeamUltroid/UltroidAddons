ed9c86acaf952919ad740b8034fa6c0673dcdbae
TrueSaiyan <ufperth@protonmail.com>
2024-01-25 14:13:56 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 # Ported From DarkCobra Originally By UNIBORG
-#
+# Fixed by @TrueSaiyan
 # Ultroid - UserBot
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
@@ -9,9 +9,11 @@
 """
 ✘ Commands Available -
 
-• `{i}test`
+• `{i}speedtst` Displays in Text
     Test Your Server Speed.
 
+• `{i}speedtest Displays in Image
+    Test Your Server Speed.
 """
 
 from datetime import datetime
@@ -21,15 +23,15 @@ import speedtest
 from . import *
 
 
-@ultroid_cmd(pattern="test ?(.*)")
+@nimbus_cmd(pattern="speedtst ?(.*)")
 async def _(event):
     input_str = event.pattern_match.group(1)
     as_document = None
-    if input_str == "image":
-        as_document = False
-    elif input_str == "file":
+    if input_str == "file":
         as_document = True
-    xx = await event.eor("`Calculating ur Ultroid Server Speed. Please wait!`")
+    elif input_str == "image":
+        as_document = False
+    xx = await event.eor("`Calculating Nimbus Server Speed. Please wait!`")
     start = datetime.now()
     s = speedtest.Speedtest()
     s.get_best_server()
@@ -52,7 +54,7 @@ async def _(event):
         speedtest_image = response
         if as_document is None:
             await xx.edit(
-                """`Ultroid Server Speed in {} sec`
+                """`Nimbus Server Speed in {} sec`
 
 `Download: {}`
 `Upload: {}`
@@ -71,7 +73,7 @@ async def _(event):
             await event.client.send_file(
                 event.chat_id,
                 speedtest_image,
-                caption="**SpeedTest** completed in {} seconds".format(ms),
+                caption=f"**SpeedTest** completed in {ms} seconds",
                 force_document=as_document,
                 reply_to=reply_msg_id,
                 allow_cache=False,
@@ -94,3 +96,82 @@ __With the Following ERRORs__
                 str(exc),
             )
         )
+
+
+temxt = (
+    "**Nimbus Speedtest completed in {0} seconds.**\n\n"
+    "**Download:**  `{1}` \n"
+    "**Upload:**  `{2}` \n"
+    "**Ping:**  `{3} ms` \n"
+    "**Internet Provider:**  `{4} ({5})` \n"
+)
+
+
+@nimbus_cmd(pattern="speedtest ?(.*)")
+async def speemdtest(event):
+    args = event.pattern_match.group(1)
+    xx = await eor(event, "`Calculating Nimbus Server Speed. Please wait!``")
+    start = datetime.now()
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()  # dchehe
+    end = datetime.now()
+    ms = (end - start).seconds
+    response = s.results.dict()
+
+    download_speed = response.get("download")
+    upload_speed = response.get("upload")
+    ping_time = response.get("ping")
+    client_infos = response.get("client")
+    i_s_p = client_infos.get("isp")
+    i_s_p_rating = client_infos.get("isprating")
+
+    if args and args == "text":
+        await xx.edit(temxt.format(
+            ms,
+            convert_from_bytes(download_speed),
+            convert_from_bytes(upload_speed),
+            ping_time,
+            i_s_p,
+            i_s_p_rating,
+        ))
+    else:
+        try:
+            speedtest_image = s.results.share()
+            await event.client.send_file(
+                event.chat_id,
+                speedtest_image,  # heeehe
+                caption="**SpeedTest** completed in {} seconds".format(ms),
+                force_document=False,
+                reply_to=event.reply_to_msg_id,
+                allow_cache=False
+            )
+            await xx.delete()
+        except Exception as exc:  # dc
+            xx2 = temxt.format(
+                ms,
+                convert_from_bytes(download_speed),
+                convert_from_bytes(upload_speed),
+                ping_time,
+                i_s_p,
+                i_s_p_rating,
+            )
+            return await xx.edit(
+                f"{xx2} \n**Exception:** `{exc}`"
+            )
+
+
+def convert_from_bytes(size):
+    power = 2 ** 10
+    n = 0
+    units = {
+        0: "",
+        1: "Kilobytes/s",
+        2: "Megabytes/s",
+        3: "Gigabytes/s",
+        4: "Terabytes/s"}
+    while size > power:
+        size /= power
+        n += 1
+    return f"{round(size, 2)} {units[n]}"
